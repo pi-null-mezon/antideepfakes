e6c3eeb2ae705c992ffc210c71a4df5269a1bb4c
pi-null-mezon <pi-null-mezon@yandex.ru>
2023-11-18 11:15:44 +0300

    updt

--- sequence/preparedata/preparedata.py
+++ sequence/preparedata/preparedata.py
@@ -37,20 +37,34 @@ def spawn(source_dir: str, target_dir: str, videos: bool, args: list):
     return p
 
 
-crop_size = 224  # 224
-iod_dst = 90     # 90
-vshift = -0.2    # -0.2
+crop_size = 256  # 224
+iod_dst = 60     # 90
+vshift = -0.1    # -0.2
 
 crop_format = f"{crop_size}x{iod_dst}x{-vshift}"
 target_location = f"/home/alex/Fastdata/deepfakes/sequence/{crop_format}"
 
 procs = list()
-conf = config(dthresh=0.9, strobe=17, tside=crop_size, teyes=iod_dst, vshift=vshift)
-'''
+conf = config(dthresh=0.9, strobe=7, tside=crop_size, teyes=iod_dst, vshift=vshift)
+
+dataset_name = 'roop'
+procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/moves/live", f"{target_location}/{dataset_name}/live", True, conf))
+procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/moves/fake", f"{target_location}/{dataset_name}/fake", True, conf))
+procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/nums/live", f"{target_location}/{dataset_name}/live", True, conf))
+procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/nums/fake", f"{target_location}/{dataset_name}/fake", True, conf))
+for proc in procs:
+    proc.wait()
+    print(f" - process {proc.pid} finished, return code: {proc.returncode}")
+procs = list()
+
 dataset_name = 'Celeb-DF-v2'
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/Celeb-real", f"{target_location}/{dataset_name}/live", True, conf))
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/YouTube-real", f"{target_location}/{dataset_name}/live", True, conf))
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/Celeb-synthesis", f"{target_location}/{dataset_name}/fake", True, conf))
+for proc in procs:
+    proc.wait()
+    print(f" - process {proc.pid} finished, return code: {proc.returncode}")
+procs = list()
 
 dataset_name = 'FaceForensics++'
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/original_sequences/actors/c23/videos", f"{target_location}/{dataset_name}/live", True, conf))
@@ -60,24 +74,25 @@ procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_se
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_sequences/Face2Face/c23/videos", f"{target_location}/{dataset_name}/fake", True, conf))
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_sequences/FaceShifter/c23/videos", f"{target_location}/{dataset_name}/fake", True, conf))
 procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_sequences/FaceSwap/c23/videos", f"{target_location}/{dataset_name}/fake", True, conf))
-procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_sequences/NeuralTextures/c23/videos", f"{target_location}/{dataset_name}/fake", True, conf))
-procs.append(spawn(f"/media/alex/Datastorage/Testdata/Video/distortion_liveness/all/live", f"{target_location}/{dataset_name}/live", True, conf))
-
+#procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/manipulated_sequences/NeuralTextures/c23/videos", f"{target_location}/{dataset_name}/fake", True, conf))
 for proc in procs:
     proc.wait()
     print(f" - process {proc.pid} finished, return code: {proc.returncode}")
 procs = list()
 
-for part in ['0', '1', '3', '5', '07', '11', '17', '23', '41', '43', '47']:
-    dataset_name = f'dfdc/train_part_{part}'
-    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/live", f"{target_location}/{dataset_name}/live", True, conf))
-    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/fake", f"{target_location}/{dataset_name}/fake", True, conf))
-'''
+dataset_name = "dfdc"
+for part in ['0', '1', '3', '5', '07', '11']:
+    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/train_part_{part}/live", f"{target_location}/{dataset_name}/live", True, conf))
+    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/train_part_{part}/fake", f"{target_location}/{dataset_name}/fake", True, conf))
+for proc in procs:
+    proc.wait()
+    print(f" - process {proc.pid} finished, return code: {proc.returncode}")
+procs = list()
+procs.append(spawn("/media/alex/Datastorage/Testdata/Video/distortion_liveness/all/live", f"{target_location}/{dataset_name}/live", True, conf))
+for part in ['17', '23', '41', '43', '47']:
+    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/train_part_{part}/live", f"{target_location}/{dataset_name}/live", True, conf))
+    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/train_part_{part}/fake", f"{target_location}/{dataset_name}/fake", True, conf))
 
-for part in ['0', '1', '3', '5', '11', '23', '41', '47']:
-    dataset_name = f'dfdc/train_part_{part}'
-    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/live", f"{target_location}/{dataset_name}/live", True, conf))
-    procs.append(spawn(f"/media/alex/HDD1_2T/Deepfakes/{dataset_name}/fake", f"{target_location}/{dataset_name}/fake", True, conf))
 
 for proc in procs:
     proc.wait()
