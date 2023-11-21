ac3a099eca495fa0aaca452c5b1b61d8659a298c
skycat8 <esdemina7@gmail.com>
2023-11-21 21:03:44 +0300

    triplet_training

---
+++ sequence_triplet/dataset.py
@@ -0,0 +1,278 @@
+import albumentations as A
+from time import perf_counter
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+import cv2
+import os
+import sys
+import pickle 
+import random
+
+
+def normalize_image(bgr, mean, std, swap_red_blue=False):
+    tmp = bgr.astype(dtype=np.float32) / 255.0
+    if swap_red_blue:
+        tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2RGB)
+    tmp -= np.asarray(mean, dtype=np.float32).reshape(1, 1, 3)
+    tmp /= np.asarray(std, dtype=np.float32).reshape(1, 1, 3)
+    return tmp
+
+
+def image2tensor(bgr, mean, std, swap_red_blue=False):
+    tmp = normalize_image(bgr, mean, std, swap_red_blue)
+    return np.transpose(tmp, axes=(2, 0, 1))  # HxWxC -> CxHxW
+
+
+class CustomDataSet(Dataset):
+    def __init__(self, paths, tsize, do_aug, mean=None, std=None, min_sequence_length=1):
+        """
+            :param tsize: target size of images
+            :param do_aug: enable augmentations
+            :param mean: values to substract
+            :param std: values to normalize on
+        """
+        if mean is None:
+            self.mean = [0.485, 0.456, 0.406]
+        else:
+            self.mean = mean
+        if std is None:
+            self.std = [0.229, 0.224, 0.225]
+        else:
+            self.std = std
+        self.tsize = tsize
+        self.do_aug = do_aug
+        self.samples = []
+        self.targets = []
+        for path in paths:
+            self.labels_list = [s.name for s in os.scandir(path) if s.is_dir()]
+            self.labels_list.sort()
+            for i, label in enumerate(self.labels_list):
+                for subdir in [s.name for s in os.scandir(os.path.join(path, label)) if s.is_dir()]:
+                    frames = [os.path.join(path, label, subdir, f.name) for f in os.scandir(os.path.join(path, label, subdir))
+                         if (f.is_file() and ('.jp' in f.name or '.pn' in f.name))]
+                    if len(frames) >= min_sequence_length:
+                        self.samples.append((i, frames[:min_sequence_length]))
+                        self.targets.append(i)
+        self.album = A.Compose([
+            A.RandomBrightnessContrast(p=0.25, brightness_limit=(-0.25, 0.25)),
+            A.HorizontalFlip(p=0.5),
+            A.CoarseDropout(p=0.75,
+                            max_holes=3,
+                            min_width=int(self.tsize[1] / 7),
+                            max_width=int(self.tsize[1] / 5),
+                            min_height=int(self.tsize[0] / 7),
+                            max_height=int(self.tsize[0] / 5)),
+            A.Affine(p=1.0,
+                     scale=(0.95, 1.05),
+                     translate_percent=(-0.05, 0.05),
+                     rotate=(-5, 5)),
+            A.GaussNoise(p=0.5, var_limit=(1, 16)),
+            A.ColorJitter(p=0.5)
+        ], p=1.0, additional_targets=dict([(f'image{i}', 'image') for i in range(min_sequence_length-1)]))
+
+    def labels_names(self):
+        d = {}
+        for i, label in enumerate(self.labels_list):
+            d[i] = label
+        return d
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        tensor = []
+        mats = {}
+        i = 0
+        for filename in sorted(self.samples[idx][1]):
+            mat = cv2.imread(filename, cv2.IMREAD_COLOR)
+
+            if mat.shape[0] != self.tsize[0] and mat.shape[1] != self.tsize[1]:
+                interp = cv2.INTER_LINEAR if mat.shape[0]*mat.shape[1] > self.tsize[0]*self.tsize[1] else cv2.INTER_CUBIC
+                mat = cv2.resize(mat, self.tsize, interpolation=interp)
+            if i == 0:
+                mats['image'] = mat
+            else:
+                mats[f'image{i-1}'] = mat
+            i += 1
+
+        if self.do_aug:
+            mats = self.album(**mats)
+
+        for key in mats:
+            # Visual control
+            #cv2.imshow("probe", mats[key])
+            #cv2.waitKey(0)
+            tensor.append(image2tensor(mats[key], mean=self.mean, std=self.std, swap_red_blue=True))
+        tensor = np.stack(tensor)
+        return torch.from_numpy(tensor), self.samples[idx][0]
+    
+
+class CustomTripletDataSet(Dataset):
+    def __init__(self, paths, tsize, do_aug, mean=None, std=None, min_sequence_length=None, balanced=False):
+        """
+            :param tsize: target size of images
+            :param do_aug: enable augmentations
+            :param mean: values to substract
+            :param std: values to normalize on
+        """
+        if mean is None:
+            self.mean = [0.485, 0.456, 0.406]
+        else:
+            self.mean = mean
+        if std is None:
+            self.std = [0.229, 0.224, 0.225]
+        else:
+            self.std = std
+        self.tsize = tsize
+        self.do_aug = do_aug
+        self.paths = paths
+        self.seq_length = min_sequence_length
+        self.balanced = balanced
+        self.generate_triplets()
+        self.album = A.Compose([
+            A.RandomBrightnessContrast(p=0.1, brightness_limit=(-0.1, 0.1)),
+            A.HorizontalFlip(p=0.5),
+            A.CoarseDropout(p=0.1,
+                            max_holes=3,
+                            min_width=int(self.tsize[1] / 7),
+                            max_width=int(self.tsize[1] / 5),
+                            min_height=int(self.tsize[0] / 7),
+                            max_height=int(self.tsize[0] / 5)),
+            A.Affine(p=1.0,
+                     scale=(0.95, 1.05),
+                     translate_percent=(-0.05, 0.05),
+                     rotate=(-5, 5)),
+            A.GaussNoise(p=0.1, var_limit=(1, 8)),
+            A.ColorJitter(p=0.1)
+        ], p=1.0, additional_targets=dict([(f'image{i}', 'image') for i in range(min_sequence_length-1)]))
+    
+    def generate_triplets(self):
+
+        triplets = []
+        for j, path in enumerate(self.paths):
+            fakes_path = os.path.join(path, 'fake')
+            fakes_seqs = os.listdir(fakes_path)
+            lives_path = os.path.join(path, 'live')
+            lives_seqs = os.listdir(lives_path)
+            live_id2folder = dict((x.split('_')[0] if '_' in x else x.split('-@-')[0], x) for x in lives_seqs)
+            if not self.balanced:
+                #TODO fix triplets generation for roop 
+                t_s = list(map(lambda x: x.split('-@-')[0], fakes_seqs))        
+                triplets.extend(list(map(lambda x: tuple([live_id2folder[y] for y in x.split('_')[:2]] + [x, 1, j]), t_s)))
+            else:
+                for i, fake_seq in enumerate(fakes_seqs):
+                    source_person_id, target_person_id = fake_seq.split('-@-')[0].split('_')[:2]
+                    if random.random() > 0.5:
+                        anch = live_id2folder[source_person_id] if source_person_id in live_id2folder else random.choice(lives_seqs)
+                        pos = live_id2folder[target_person_id] if target_person_id in live_id2folder else random.choice(lives_seqs)
+                        triplets.append((anch, pos, fake_seq, 1, j))
+                    else:
+                        neg_id = random.choice([source_person_id, target_person_id])
+                        pos_samples_with_same_id = list(filter(lambda x: (neg_id in x) and (x!=fake_seq), fakes_seqs))
+                        if len(pos_samples_with_same_id):
+                            pos_sample = random.choice(pos_samples_with_same_id)
+                        else:    
+                            pos_sample = random.choice(fakes_seqs)
+                        neg_sample = live_id2folder[neg_id] if neg_id in live_id2folder else random.choice(lives_seqs)
+                        triplets.append((fake_seq, pos_sample, neg_sample, 0, j))
+        
+        self.triplets = triplets
+
+    def __len__(self):
+        return len(self.triplets)
+
+    def __getitem__(self, idx):
+
+        anch, pos, neg, anch_cls, path_id = self.triplets[idx]
+        path = self.paths[path_id]
+        anch_seq = self.load_seq(anch, anch_cls, path)
+        pos_seq = self.load_seq(pos, anch_cls, path)
+        neg_seq = self.load_seq(neg, 1 - anch_cls, path)
+
+        return anch_seq, pos_seq, neg_seq, anch_cls
+
+    def load_seq(self, folder, label, path):
+        
+        folder_path = os.path.join(path, 'live' if label else 'fake', folder)
+        filenames = os.listdir(folder_path)
+        tensor = []
+        mats = dict()
+        if len(filenames) > self.seq_length:
+            start_idx = random.randint(0, len(filenames) - self.seq_length)
+            end_idx = start_idx + self.seq_length
+            filenames = filenames[start_idx: end_idx]
+        elif len(filenames) < self.seq_length:
+            while len(filenames) < self.seq_length:
+                filenames += filenames
+            filenames = filenames[:self.seq_length]
+
+        for i, filename in enumerate(filenames):
+            file_path = os.path.join(folder_path, filename)
+            mat = cv2.imread(file_path, cv2.IMREAD_COLOR)
+
+            if mat.shape[0] != self.tsize[0] and mat.shape[1] != self.tsize[1]:
+                interp = cv2.INTER_LINEAR if mat.shape[0]*mat.shape[1] > self.tsize[0]*self.tsize[1] else cv2.INTER_CUBIC
+                mat = cv2.resize(mat, self.tsize, interpolation=interp)
+
+            if i == 0:
+                mats['image'] = mat
+            else:
+                mats[f'image{i-1}'] = mat
+        
+        if self.do_aug:
+            mats = self.album(**mats)
+
+        for key in mats:
+            tensor.append(image2tensor(mats[key], mean=self.mean, std=self.std, swap_red_blue=True))
+        tensor = np.stack(tensor)
+        
+        return torch.from_numpy(tensor)
+    
+    def get_image(self, file_path):
+
+        mat = cv2.imread(file_path, cv2.IMREAD_COLOR)
+        mat = cv2.cvtColor(mat, cv2.COLOR_BGR2RGB)
+
+        if mat.shape[0] != self.tsize[0] and mat.shape[1] != self.tsize[1]:
+            interp = cv2.INTER_LINEAR if mat.shape[0]*mat.shape[1] > self.tsize[0]*self.tsize[1] else cv2.INTER_CUBIC
+            mat = cv2.resize(mat, self.tsize, interpolation=interp)
+
+        if self.do_aug:
+            mat = self.album(image=mat)["image"]
+
+        return torch.from_numpy(image2tensor(mat, mean=self.mean, std=self.std))
+
+
+
+class Speedometer:
+    def __init__(self, gamma: float = 0.9):
+        self.gamma = gamma
+        self._speed = None
+        self.t0 = perf_counter()
+
+    def reset(self):
+        self._speed = None
+        self.t0 = perf_counter()
+
+    def update(self, samples):
+        if self._speed is None:
+            self._speed = samples / (perf_counter() - self.t0)
+        else:
+            self._speed = self._speed * self.gamma + (1 - self.gamma) * samples / (perf_counter() - self.t0)
+        self.t0 = perf_counter()
+
+    def speed(self):
+        return self._speed
+
+
+
+if __name__ == '__main__':
+    dataset = CustomTripletDataSet(['../sequence/256x60x0.1/toloka'], 
+                                   tsize=(256, 256), 
+                                   do_aug=True, 
+                                   min_sequence_length=10, 
+                                   balanced=True)
+    print(dataset.triplets[:2])
+    anch, pos, neg, anch_cls = dataset.__getitem__(0)
+    print(anch.shape, pos.shape, neg.shape, anch_cls)
---
+++ sequence_triplet/encoder.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+
+
+class MultiHeadAttention(nn.Module):  # https://www.datacamp.com/tutorial/building-a-transformer-with-py-torch
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        # Ensure that the model dimension (d_model) is divisible by the number of heads
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
+        # Initialize dimensions
+        self.d_model = d_model  # Model's dimension
+        self.num_heads = num_heads  # Number of attention heads
+        self.d_k = d_model // num_heads  # Dimension of each head's key, query, and value
+        self.scale = self.d_k ** 0.5
+
+        # Linear layers for transforming inputs
+        self.W_q = nn.Linear(d_model, d_model)  # Query transformation
+        self.W_k = nn.Linear(d_model, d_model)  # Key transformation
+        self.W_v = nn.Linear(d_model, d_model)  # Value transformation
+        self.W_o = nn.Linear(d_model, d_model)  # Output transformation
+
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        # Calculate attention scores
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / self.scale
+
+        # Apply mask if provided (useful for preventing attention to certain parts like padding)
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+
+        # Softmax is applied to obtain attention probabilities
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+
+        # Multiply by values to obtain the final output
+        output = torch.matmul(attn_probs, V)
+        return output
+
+    def split_heads(self, x):
+        # Reshape the input to have num_heads for multi-head attention
+        batch_size, seq_length, d_model = x.size()
+        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
+
+    def combine_heads(self, x):
+        # Combine the multiple heads back to original shape
+        batch_size, _, seq_length, d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+
+    def forward(self, Q, K, V, mask=None):
+        # Apply linear transformations and split heads
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+
+        # Perform scaled dot-product attention
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+
+        # Combine heads and apply output transformation
+        output = self.W_o(self.combine_heads(attn_output))
+        return output
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-9.210340371976184 / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        attn_output = self.self_attn(x, x, x, mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x
+
+
+class EncoderNet(nn.Module):
+    def __init__(self, d_model, num_heads, num_layers, d_ff, dropout_l, dropout, num_classes, max_seq_length):
+        super(EncoderNet, self).__init__()
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+        self.layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout_l) for _ in range(num_layers)])
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(d_model, num_classes)
+
+    def forward(self, x):
+        # dimensions of x: batch_size, sequence_length, features_size
+        x = self.positional_encoding(x)
+        for layer in self.layers:
+            x = layer(x)
+        features = self.dropout(x)
+        x = self.fc(features)
+        x = torch.mean(x, dim=1)  # average across sequence length
+        return x, features.mean(dim=1)
+
+
+class DeepfakeDetector(nn.Module):
+    def __init__(self, single_shot, encoder):
+        super(DeepfakeDetector, self).__init__()
+        self.ss = single_shot
+        self.enc = encoder
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        # dimensions of x: batch_size, sequence_length, channels, height, width
+        batch_size, seq_length = x.shape[0], x.shape[1]
+        y = self.ss(x.view(-1, x.shape[-3], x.shape[-2], x.shape[-1])).view(batch_size, seq_length, -1)
+        y = self.enc(y)
+        y = self.softmax(y)
+        return y
+
+
+if __name__ == "__main__":
+    torch.manual_seed(2308)
+
+    batch_size = 3
+    sequence_length = 50
+    features_size = 1280
+
+    model = EncoderNet(d_model=features_size, num_heads=4, num_layers=1, d_ff=2048, dropout=0.1, num_classes=2)
+    model.eval()
+    input = torch.normal(mean=0.0, std=1.0, size=(batch_size, sequence_length, features_size))
+
+    with torch.no_grad():
+        output = model(input)
+        print(output)
